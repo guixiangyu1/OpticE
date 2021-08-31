3a2146122f921d4e03e3d86f8113b9010669fe3c
guixiangyu <guixiangyu1@gmail.com>
2021-08-31 09:21:47 +0800

    init-step

--- codes/model.py
+++ codes/model.py
@@ -58,6 +58,7 @@ class KGEModel(nn.Module):
 
         # if model_name == 'pRotatE' or model_name=='Ring':
         #     self.modulus = nn.Parameter(torch.Tensor([[0.5 * self.embedding_range.item()]]))
+        self.modulus = nn.Parameter(torch.Tensor([[0.5 * self.embedding_range.item()]]))
 
         # Do not forget to modify this line when you add a new model in the "forward" function
         if model_name not in ['TransE', 'DistMult', 'ComplEx', 'RotatE', 'pRotatE', 'Ring', 'Ellipse']:
