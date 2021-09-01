e55edf548c49abf476e769b499c46619d2974244
guixiangyu <guixiangyu1@gmail.com>
2021-09-01 14:50:57 +0800

    init-step

--- codes/model.py
+++ codes/model.py
@@ -281,14 +281,14 @@ class KGEModel(nn.Module):
         hr = phase_h + r1
         tr = phase_t + r2
 
-        # x = (torch.sin(hr) ** 2 + 4 * torch.cos(hr) ** 2)
-        # y = (torch.sin(tr) ** 2 + 4 * torch.cos(tr) ** 2)
-
-        x = 1 / (1 - 0.1 * torch.cos(hr) ** 2)
-        y = 1 / (1 - 0.1 * torch.cos(tr) ** 2)
+        x = (torch.sin(hr) ** 2 + 4 * torch.cos(hr) ** 2)
+        y = (torch.sin(tr) ** 2 + 4 * torch.cos(tr) ** 2)
+        #
+        # x = 1 / (1 - 0.8 * torch.cos(hr) ** 2)
+        # y = 1 / (1 - 0.8 * torch.cos(tr) ** 2)
 
         xy = x + y - 2 * torch.sqrt(x * y) * torch.cos(hr - tr)
-        score = self.gamma.item() - xy.sum(dim=2) * self.modulus
+        score = self.gamma.item() - xy.sum(dim=2) * 0.0025
         return score
 
     @staticmethod
